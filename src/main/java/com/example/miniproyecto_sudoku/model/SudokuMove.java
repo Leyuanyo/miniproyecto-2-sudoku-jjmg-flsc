4d0bc15a64a44456d5b5e5c0fa30b0859aaff3db
lmdshow <frannsito110@gmail.com>
2026-05-24 16:57:36 -0500

    feat: add SudokuMove class

---
+++ src/main/java/com/example/miniproyecto_sudoku/model/SudokuMove.java
@@ -0,0 +1,21 @@
+package com.example.miniproyecto_sudoku.model;
+
+public class SudokuMove {
+
+    private final int row;
+    private final int col;
+    private final int previousValue;
+    private final int newValue;
+
+    public SudokuMove(int row, int col, int previousValue, int newValue) {
+        this.row = row;
+        this.col = col;
+        this.previousValue = previousValue;
+        this.newValue = newValue;
+    }
+
+    public int getRow() { return row; }
+    public int getCol() { return col; }
+    public int getPreviousValue() { return previousValue; }
+    public int getNewValue() { return newValue; }
+}
