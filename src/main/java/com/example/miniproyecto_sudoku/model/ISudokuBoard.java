9e7eece17a2ee5edcf1e03385934c90a9a5cff70
lmdshow <frannsito110@gmail.com>
2026-05-24 17:57:38 -0500

    feat: add ISudokuBoard interface

---
+++ src/main/java/com/example/miniproyecto_sudoku/model/ISudokuBoard.java
@@ -0,0 +1,18 @@
+package com.example.miniproyecto_sudoku.model;
+
+public interface ISudokuBoard {
+
+    int getCell(int row, int col);
+
+    void setCell(int row, int col, int value);
+
+    void setFixed(int row, int col, int value);
+
+    boolean isFixed(int row, int col);
+
+    int[][] getGridCopy();
+
+    SudokuGameState getGameState();
+
+    void setGameState(SudokuGameState gameState);
+}
